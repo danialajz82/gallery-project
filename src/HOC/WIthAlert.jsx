03a1704dd4dc073b8aff515ae9640c7d6a506d5e
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2023-11-20 13:41:18 +0330

    HOC

---
+++ src/HOC/WIthAlert.jsx
@@ -0,0 +1,22 @@
+// import React, { useState } from "react";
+// import swal from "sweetalert";
+
+// const WithAlert = (MainComponent) => {
+//   const NewComponent = (props) => {
+//     const Confrim = (message) => {
+//       return swal({
+//         title: "حذف رکورد",
+//         text: message,
+//         icon: "warning",
+//         buttons: ["خیر", "بله"],
+//         dangerMode: true,
+//       });
+//     };
+
+//     return <MainComponent {...props} Confrim={Confrim} />;
+//   };
+
+//   return NewComponent;
+// };
+
+// export default WithAlert;
